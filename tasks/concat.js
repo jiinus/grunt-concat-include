fd616baf7a370e5544bbc56e18f767b28f2ce515
Tobie Morgan Hitchcock <tobie@abcum.com>
2014-08-26 15:48:39 +0100

    Don't quote filenames to stdout

--- tasks/concat.js
+++ tasks/concat.js
@@ -83,7 +83,7 @@ module.exports = function(grunt) {
 			grunt.file.write(f.dest, banner + src + footer);
 
 			// Print a success message.
-			grunt.log.writeln('File "' + f.dest + '" created.');
+			grunt.log.writeln('File ' + f.dest + ' created.');
 
 		});
 
